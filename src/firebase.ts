bdd8b163804e97982ab0c65ddd0fb1ef5b863316
Ethen1264 <jxorhks86909763@gmail.com>
2024-01-11 20:03:53 +0900

    add firebase

---
+++ src/firebase.ts
@@ -0,0 +1,10 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyBAMK1xpJNBRaUv282gYf9T-TvLgFqRBcc",
+  authDomain: "nwitter-reloaded-914a9.firebaseapp.com",
+  projectId: "nwitter-reloaded-914a9",
+  storageBucket: "nwitter-reloaded-914a9.appspot.com",
+  messagingSenderId: "206489839192",
+  appId: "1:206489839192:web:4eadd5c8071f741a5f0c2d"
+};
+const app = initializeApp(firebaseConfig);
