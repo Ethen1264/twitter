b415360147ba1d34e7a9a488a3910d130079769f
Ethen1264 <jxorhks86909763@gmail.com>
2024-01-14 10:12:08 +0900

    add navigation bar

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,12 +1,8 @@
 import { auth } from '../firebase';
 
 export default function Home() {
-  const logOut = () => {
-    auth.signOut()      // 로그아웃 함수
-  }
   return (
     <h1>
-      <button onClick={logOut}>Log Out</button>        {/*로그아웃*/}
-    </h1>
+      
   );
 }
