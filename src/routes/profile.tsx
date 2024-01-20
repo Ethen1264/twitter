f496a07b8a97d5483a6a3b128e74e56c3cefb64e
Ethen1264 <jxorhks86909763@gmail.com>
2024-01-21 01:53:24 +0900

    add profile

--- src/routes/profile.tsx
+++ src/routes/profile.tsx
@@ -1,14 +1,25 @@
-import styled from 'styled-components';
-import { auth, storage } from '../firebase';
-import { useState } from 'react';
-import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
+import { styled } from "styled-components";
+import { auth, db, storage } from "../firebase";
+import { useEffect, useState } from "react";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import { updateProfile } from "firebase/auth";
+import {
+  collection,
+  getDocs,
+  limit,
+  orderBy,
+  query,
+  where,
+} from "firebase/firestore";
+import { ITweet } from "../components/timeline";
+import Tweet from "../components/tweet";
 
 const Wrapper = styled.div`
-display: flex;
-align-items: center;
-flex-direction: column;
-gap: 20px;
-`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  gap: 20px;
+`;
 const AvatarUpload = styled.label`
   width: 80px;
   overflow: hidden;
@@ -19,24 +30,32 @@ const AvatarUpload = styled.label`
   display: flex;
   justify-content: center;
   align-items: center;
-  svg{
+  svg {
     width: 50px;
   }
-`
+`;
+
 const AvatarImg = styled.img`
   width: 100%;
-
-`
+`;
 const AvatarInput = styled.input`
-display: none;
-`
+  display: none;
+`;
 const Name = styled.span`
   font-size: 22px;
-`
+`;
+
+const Tweets = styled.div`
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  gap: 10px;
+`;
 
 export default function Profile() {
   const user = auth.currentUser;
-  const [avatar, setAvatar] = useState(user?.photoURL)
+  const [avatar, setAvatar] = useState(user?.photoURL);
+  const [tweets, setTweets] = useState<ITweet[]>([]);
   const onAvatarChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
     const { files } = e.target;
     if (!user) return;
@@ -51,16 +70,58 @@ export default function Profile() {
       });
     }
   };
+  const fetchTweets = async () => {
+    const tweetQuery = query(
+      collection(db, "tweets"),
+      where("userId", "==", user?.uid),
+      orderBy("createdAt", "desc"),
+      limit(25)
+    );
+    const snapshot = await getDocs(tweetQuery);
+    const tweets = snapshot.docs.map((doc) => {
+      const { tweet, createdAt, userId, username, photo } = doc.data();
+      return {
+        tweet,
+        createdAt,
+        userId,
+        username,
+        photo,
+        id: doc.id,
+      };
+    });
+    setTweets(tweets);
+  };
+  useEffect(() => {
+    fetchTweets();
+  }, []);
   return (
     <Wrapper>
-      <AvatarUpload htmlFor='avatar'>
-        {Boolean(avatar) ? <AvatarImg src={avatar} />:<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-  <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
-</svg>
-}
+      <AvatarUpload htmlFor="avatar">
+        {avatar ? (
+          <AvatarImg src={avatar} />
+        ) : (
+          <svg
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+            aria-hidden="true"
+          >
+            <path d="M10 8a3 3 0 100-6 3 3 0 000 6zM3.465 14.493a1.23 1.23 0 00.41 1.412A9.957 9.957 0 0010 18c2.31 0 4.438-.784 6.131-2.1.43-.333.604-.903.408-1.41a7.002 7.002 0 00-13.074.003z" />
+          </svg>
+        )}
       </AvatarUpload>
-      <AvatarInput onChange={onAvatarChange} id='avatar' type="file" accept="image/*" />
-      <Name>{user?.displayName ?? 'Anonymous'}</Name>
+      <AvatarInput
+        onChange={onAvatarChange}
+        id="avatar"
+        type="file"
+        accept="image/*"
+      />
+      <Name>{user?.displayName ?? "Anonymous"}</Name>
+      <Tweets>
+        {tweets.map((tweet) => (
+          <Tweet key={tweet.id} {...tweet} />
+        ))}
+      </Tweets>
     </Wrapper>
   );
 }
